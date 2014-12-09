c73a46bdc14081527fea0a0746e147569fc240b1
ati-ozgur <ati.ozgur@gmail.com>
2014-12-09 18:23:54 +0200

    Some extra code

--- R/caretExamples/JournalOfStatisticalSoftwareExample1.R
+++ R/caretExamples/JournalOfStatisticalSoftwareExample1.R
@@ -1,3 +1,6 @@
+
+
+
 library("caret")
 set.seed(1)
 inTrain <- createDataPartition(mutagen, p = 3/4, list = FALSE)
@@ -8,3 +11,15 @@ trainClass <- mutagen[inTrain]
 testClass <- mutagen[-inTrain]
 
 prop.table(table(mutagen))
+
+# 
+# ncol(trainDescr)
+# descrCorr <- cor(trainDescr)
+# highCorr <- findCorrelation(descrCorr, 0.90)
+# trainDescr <- trainDescr[, -highCorr]
+# testDescr <- testDescr[, -highCorr]
+# ncol(trainDescr)
+
+xTrans <- preProcess(trainDescr)
+trainDescr <- predict(xTrans, trainDescr)
+testDescr <- predict(xTrans, testDescr)
