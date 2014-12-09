c2ea04aef47ba7f135fffae50a009414b0f8d56a
ati-ozgur <ati.ozgur@gmail.com>
2014-12-09 14:03:31 +0200

    utf8 example

--- R/utf8/utf8Example1.R
+++ R/utf8/utf8Example1.R
@@ -1,6 +1,8 @@
-s = c("aaşğiü", "ŞÇÖĞÜbb") 
+http://stackoverflow.com/questions/27373882/extracting-dataframe-into-a-utf-8-csv
+
+s = c("aaşğiü", "Đối kháng")
 df = data.frame(s) 
 
-con = file("myFile.csv","w", encoding="utf8")
+con = file("myFile.csv","w", encoding="UTF-8")
 write.csv(df, con, row.names=FALSE)
 close(con)
